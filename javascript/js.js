1b7e742687f611434c27856dd0def744718903fa
Piotr Krzyzanowski <pinowski92@gmail.com>
2022-10-23 21:51:58 +0200

    Added calculating function

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,24 @@
 {
   const formElement = document.querySelector(".js-form");
+
+  const calculateResult = (amount, currency) => {
+    const eur = 0.21;
+    const gbp = 0.18;
+    const usd = 0.21;
+    const dkk = 1.55;
+
+    switch (currency) {
+      case "EUR":
+        return amount * eur;
+
+      case "GBP":
+        return amount * gbp;
+
+      case "USD":
+        return amount * usd;
+
+      case "DKK":
+        return amount * dkk;
+    }
+  };
 }
